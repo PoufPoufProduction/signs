4fa83fd06260072aa2243c68195695f83c6e89e4
Johann C <johannc@poufpoufproduction.fr>
2018-08-28 11:33:51 +0200

    new signs and enhanced export script

--- export.sh
+++ export.sh
@@ -1,9 +1,87 @@
 #!/bin/sh
-echo "export all signs... processing"
-mkdir tmp
-rm -rf tmp/*
-for f in res/img/*/*.svg; do
-	inkscape $f -w 128 -h 128 -e tmp/`basename $f .svg`.png
+
+OPTIND=1
+nb=0
+lang="fr"
+width=128
+tile=5
+fontsize=12
+
+while getopts "h?n:l:w:t:s:" opt; do
+    case "$opt" in
+    h|\?)
+        echo usage: $0 -n number -l lang -w width -t nb tiles -s fontsize
+        exit 0
+        ;;
+    n)  nb=$OPTARG
+        ;;
+    l)  lang=$OPTARG
+        ;;
+    w)  width=$OPTARG
+        ;;
+    t)  tile=$OPTARG
+        ;;
+    s)  fontsize=$OPTARG
+        ;;
+    esac
 done
-montage tmp/*.png -tile 5x sign.png
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+echo "export signs... processing"
+echo " + (lang: $lang) (number: $nb)"
+
+if [ ! -f "data/$lang.txt" ] ; then
+	echo " + ERROR: data/$lang.txt is unknown"
+	exit 1
+fi
+
+# GET THE SIGNS TO EXPORT
+
+cpt=0
+rm -f t_1.txt
+touch t_1.txt
+for f in `ls -1t res/img/*/*` ; do
+	name=`basename $f .svg`
+	line=`grep $name "data/$lang.txt"`
+	if [ `echo $line | wc -c` -gt 1 ] ; then
+		if [ ! $nb -eq 0 ] ; then cpt=$((cpt+1)); fi
+		if [ $cpt -le $nb ] ; then
+			echo $line >> t_1.txt
+		fi
+	else
+		echo " + INFO: $f is not referenced in data/$lang.txt"
+	fi
+done
+
+# SORT THE SIGN LIST
+
+sort t_1.txt > t_2.txt
+rm -f t_1.txt
+echo " + exporting `cat t_2.txt | wc -l` signs"
+
+# EXPORT SIGNS
+
+if [ ! -d "tmp" ] ; then mkdir tmp ; fi
+rm -rf tmp/*
+
+IFS=$'\n'
+cmd=""
+for l in `cat t_2.txt` ; do
+	label=`echo $l | sed -e "s/\([^ ]\+\) .*/\1/g"`
+	file=`echo $l | sed -e "s/[^|]*| \([^ ]\+\) .*/\1/g"`
+	name=`echo $file | sed -e "s|[^/]*/\(.*\)|\1|g"`
+	echo " + processing $label: $file (${width}x${width})"
+	echo inkscape res/img/$file.svg -w $width -h $width -e tmp/$name.png
+	cmd="$cmd -label $label tmp/$name.png"
+done
+
+echo " + building sign.png"
+echo montage$cmd -tile ${tile}x -pointsize $fontsize sign.png
+
+
+rm -f t_2.txt
 rm -rf tmp
+
